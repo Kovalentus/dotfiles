492cb442bd32196592adb14c6597aa574588d509
Joel Odd <jolly_odden_@hotmail.com>
2018-09-13 13:25:42 +0200

    Added support for multimonitor setup

--- dotfiles-laptop/i3/polybar.sh
+++ dotfiles-laptop/i3/polybar.sh
@@ -7,5 +7,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar top -r &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top &
+done
 polybar bot -r &
